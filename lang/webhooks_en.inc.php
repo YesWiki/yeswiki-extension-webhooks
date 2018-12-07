7592ccd7620d4b42dd5bc730869fb4c727329e9b
srosset81 <srosset81@gmail.com>
2018-12-08 00:01:21 +0100

    Add english translation

---
+++ lang/webhooks_en.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+$GLOBALS['translations'] = array_merge(
+    $GLOBALS['translations'],
+    array(
+        'WEBHOOKS_CONFIG_TITLE' => 'Webhooks',
+        'WEBHOOKS_CONFIG_DESC' => 'The URLs defined here will be called at every addition / edition / deletion of Bazar items.',
+        'WEBHOOKS_FORMAT_RAW' => 'Raw',
+        'WEBHOOKS_FORMAT_MATTERMOST' => 'Mattermost',
+        'WEBHOOKS_FORMAT_SLACK' => 'Slack',
+        'WEBHOOKS_URL_PLACEHOLDER' => 'URL address',
+        'WEBHOOKS_UPDATE' => 'Update',
+        'WEBHOOKS_ANONYMOUS_USER' => 'Anonymous',
+    )
+);
