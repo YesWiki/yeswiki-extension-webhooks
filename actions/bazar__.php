f0227d94ddce6c6069bbc143cd0e9d545e489b4c
srosset81 <srosset81@gmail.com>
2018-12-08 10:28:50 +0100

    Hack to prevent add webhook to be called twice

--- actions/bazar__.php
+++ actions/bazar__.php
@@ -7,22 +7,30 @@ if (!defined('WIKINI_VERSION')) {
 $GLOBALS['params'] = getAllParameters($this);
 
 switch ($GLOBALS['params']['vue']) {
+    // Display webhooks form
     case BAZ_VOIR_FORMULAIRE:
         if( !isset($GLOBALS['params']['action_formulaire']) ) {
             echo webhooks_formulaire();
         }
         break;
 
+    // Call webhook on addition
     case BAZ_VOIR_CONSULTER:
         switch($GLOBALS['params']['action']) {
             case BAZ_VOIR_FICHE:
                 if( $_GET['message']==='ajout_ok' ) {
-                    $data = baz_valeurs_fiche($_GET['id_fiche']);
-                    webhooks_post_all($data, WEBHOOKS_ACTION_ADD);
+                    // We set this condition because otherwise the page is called twice and the webhook is sent twice
+                    // TODO: Understand why the YesWiki core calls this kind of page twice
+                    if( !isset($GLOBALS['add_webhook_already_called']) ) {
+                        $data = baz_valeurs_fiche($_GET['id_fiche']);
+                        webhooks_post_all($data, WEBHOOKS_ACTION_ADD);
+                        $GLOBALS['add_webhook_already_called'] = true;
+                    }
                 }
         }
         break;
 
+    // Incoming webhook for tests
     case WEBHOOKS_VUE_TEST:
         $GLOBALS['wiki']->InsertTriple(
             $GLOBALS['wiki']->GetPageTag(),
