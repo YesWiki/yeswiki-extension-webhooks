4e5379df39ef7ec1918b3063fc38211c103bb3e7
srosset81 <srosset81@gmail.com>
2020-09-24 15:37:36 +0200

    Remove baz_valeurs_fiche, add BazarFiche::isFiche

--- handlers/page/__deletepage.php
+++ handlers/page/__deletepage.php
@@ -5,11 +5,9 @@ if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-if ($this->HasAccess('write')) {
-    $type = $this->GetTripleValue($this->GetPageTag(), 'http://outils-reseaux.org/_vocabulary/type', '', '');
+global $bazarFiche;
 
-    if ($type == 'fiche_bazar' && $_GET['confirme']==='oui') {
-        $data = json_decode($GLOBALS['wiki']->page['body'], true);
-        webhooks_post_all($data, WEBHOOKS_ACTION_DELETE);
-    }
+if ($this->HasAccess('write') && $bazarFiche->isFiche($this->GetPageTag()) && $_GET['confirme']==='oui') {
+    $data = json_decode($GLOBALS['wiki']->page['body'], true);
+    webhooks_post_all($data, WEBHOOKS_ACTION_DELETE);
 }
