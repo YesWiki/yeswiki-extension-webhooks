342775f50f475ae0a3f8279f38a076ad1c510868
srosset81 <srosset81@gmail.com>
2020-11-06 18:09:39 +0100

    Use services classes instead of aliases everywhere

--- handlers/page/__editiframe.php
+++ handlers/page/__editiframe.php
@@ -1,11 +1,12 @@
 <?php
 
-// Vérification de sécurité
+use YesWiki\Bazar\Service\FicheManager;
+
 if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-$ficheManager = $this->services->get('bazar.fiche.manager');
+$ficheManager = $this->services->get(FicheManager::class);
 
 if ($this->HasAccess('write') && $ficheManager->isFiche($this->GetPageTag()) && isset($_POST['bf_titre'])) {
     webhooks_post_all($_POST, WEBHOOKS_ACTION_EDIT);
